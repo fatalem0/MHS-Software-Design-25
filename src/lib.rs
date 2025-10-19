d2e4257b9d0318522749cfb4cb43f81720edf38a
kudiko <dkudiko@gmail.com>
2025-10-20 02:39:36 +0300

    added simple tests

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+/// Small library used to demonstrate CI
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_works() {
+        assert_eq!(add(2, 3), 5);
+    }
+
+    #[test]
+    fn add_negative() {
+        assert_eq!(add(-1, -1), -2);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let a = 2;
+    let b = 3;
+    println!("{} + {} = {}", a, b, rust_ci_example::add(a, b));
+}
