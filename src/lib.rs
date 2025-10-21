cc81f78100ba89d1e8e8637a93898ec3b3369034
kudiko <dkudiko@gmail.com>
2025-10-22 02:11:17 +0300

    lib.rs module connections

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,5 @@
-/// Small library used to demonstrate CI
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
+/// CLI Shell Library - Command Line Interface with REPL support
+pub mod modules;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn add_works() {
-        assert_eq!(add(2, 3), 5);
-    }
-
-    #[test]
-    fn add_negative() {
-        assert_eq!(add(-1, -1), -2);
-    }
-}
+pub use modules::init::Init;
+pub use modules::repl::Repl;
