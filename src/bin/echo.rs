ea1c596bc6e1acf8f33d224652d67ac78855ad19
kudiko <dkudiko@gmail.com>
2025-10-22 01:35:08 +0300

    added echo, build script & edited cargo toml

---
+++ src/bin/echo.rs
@@ -0,0 +1,13 @@
+use std::env;
+
+/// Echo command implementation as a standalone executable
+/// Prints all arguments separated by spaces to stdout
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    
+    if args.is_empty() {
+        println!();
+    } else {
+        println!("{}", args.join(" "));
+    }
+}
